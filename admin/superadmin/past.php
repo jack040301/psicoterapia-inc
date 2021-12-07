4aff0f4fbd889fc85dbfb1c212d90b64c6959465
demverleenespinola <espinola.demverleen.bscs2019@gmail.com>
2021-12-07 19:40:23 +0800

    12/07/21

--- admin/superadmin/past.php
+++ admin/superadmin/past.php
@@ -132,7 +132,7 @@
                                 $see_doctor = mysqli_query($connect, $see_doctor_query);
                                 $fetch_see_doctor = mysqli_fetch_assoc($see_doctor);
                         ?>
-                            <tr style="border-bottom:2px solid whitesmoke;"><td><?php echo $row['id']; ?></td><td><?php echo $row['date']; ?></td><td><?php echo ucwords($row['lastname']); ?></td><td><?php echo ucwords($row['firstname']); ?></td><td><?php echo number_format($row['age']); ?></td><td><?php echo $row['mobile']; ?></td><td>Doc. <?php echo ucwords($fetch_see_doctor['lastname']); ?></td><td><?php echo ucwords($row['status']); ?></td></tr>
+                            <tr style="border-bottom:2px solid whitesmoke;"><td><?php echo $row['id']; ?></td><td><?php echo $row['date']; ?></td><td><?php echo ucwords($row['lastname']); ?></td><td><?php echo ucwords($row['firstname']); ?></td><td><?php echo number_format($row['userAge']); ?></td><td><?php echo $row['mobile']; ?></td><td>Doc. <?php echo ucwords($fetch_see_doctor['lastname']); ?></td><td><?php echo ucwords($row['status']); ?></td></tr>
                         <?php } ?>
                     </tbody>
                 </table>
